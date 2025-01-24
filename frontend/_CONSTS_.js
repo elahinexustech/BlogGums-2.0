e7bb90af81396dd02db3a37523779a3d40ac2ccf
ElahiNexusTech <141256860+elahinexustech@users.noreply.github.com>
2025-01-25 01:02:19 +0500

    Update _CONSTS_.js

--- frontend/_CONSTS_.js
+++ frontend/_CONSTS_.js
@@ -1,5 +1,3 @@
-export const SERVER = "http://127.0.0.1"
-export const PORT = "8000"
 export const ACCESS_TOKEN = "ACCESS_TOKEN"
 export const REFRESH_TOKEN = "REFRESH_TOKEN"
 export const THEME_MODE = "theme"

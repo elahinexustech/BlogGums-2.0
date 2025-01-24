63d69122378c57d76ec5515e1cf613e3c0e6a0f6
MAbdullahElahi <mabdullahelahi574@gmail.com>
2025-01-25 01:06:40 +0500

    update site

---
+++ frontend/src/_CONSTS_.js
@@ -0,0 +1,8 @@
+export const SERVER = null
+export const PORT = null
+export const ACCESS_TOKEN = "ACCESS_TOKEN"
+export const REFRESH_TOKEN = "REFRESH_TOKEN"
+export const THEME_MODE = "theme"
+export const USER_DATA = 'User Data'
+export const BLOG_FONT_SIZE = 'fs'
+export const CODE_THEME = 'code theme'
